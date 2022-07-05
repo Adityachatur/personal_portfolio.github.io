6fba457122fc0a7d176ad368ce5ba4f9692efe7b
Adityachatur <66326241+Adityachatur@users.noreply.github.com>
2022-07-05 12:39:32 +0530

    personal portfolio

---
+++ js/style.js
@@ -0,0 +1,9 @@
+/***Aside 
+const nav = document.querySelector(".nav"),
+      navlist = nav.querySelectorAll("li"),
+      totalNavList = navlist.length;
+      for(let i=0; i<totalNavList; i++)
+     {
+        const a = navlist[i].querySelector("a");
+        console.log(a)
+     }*/
